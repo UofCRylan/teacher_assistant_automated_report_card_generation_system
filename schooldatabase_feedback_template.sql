783622a726406ac83a26c644ee558bfabacfd7ee
elizabethszent <elizabethszentmiklossy@gmail.com>
2025-04-21 19:30:02 -0600

    Update feedback database

--- schooldatabase_feedback_template.sql
+++ schooldatabase_feedback_template.sql
@@ -29,8 +29,6 @@ CREATE TABLE `feedback_template` (
   `comment_template` longtext,
   PRIMARY KEY (`letter`,`class_number`),
   KEY `fk_subject_idx` (`subject`),
-  KEY `fk_class_number_idx` (`class_number`),
-  CONSTRAINT `fk_classnumber` FOREIGN KEY (`class_number`) REFERENCES `class` (`class_number`),
   CONSTRAINT `fk_letter` FOREIGN KEY (`letter`) REFERENCES `final_grade` (`letter`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -54,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-20 21:15:32
+-- Dump completed on 2025-04-21 19:28:08
